a29443e379392332ceafbcfc9662dcbbce17e296
Tos1ek <antek.marszk@gmail.com>
2024-03-29 13:37:50 +0100

    sprawdzenie mono czy stero do testow

--- plap/parameterization/time.py
+++ plap/parameterization/time.py
@@ -5,18 +5,18 @@ def amplitude_envelope(audio_signal, sample_rate, block_size, overlap):
     frame_length_samples = int(block_size * sample_rate)
     step = round((100 - overlap) / 100 * frame_length_samples)
 
-    # Initialize the envelope
+   
     envelope = np.zeros_like(audio_signal)
     current_max = 0
 
     for i in range(len(audio_signal)):
-        # Update the current max if the current sample is greater
+       
         if audio_signal[i] > current_max:
             current_max = audio_signal[i]
-        # Decay the current max based on the step
+       
         elif i % step == 0:
             current_max *= (
-                0.99  # Slight decay to allow the envelope to follow the signal down
+                0.99  
             )
 
         envelope[i] = current_max
